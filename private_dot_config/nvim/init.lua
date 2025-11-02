c9f30ab42021fe5064c51c6a46d6b823d427774b
lew <lew@ily.rs>
2025-11-02 03:29:32 +0000

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -42,3 +42,15 @@ vim.o.iskeyword       = '@,48-57,_,192-255,-'
 vim.o.tabstop         = 2
 vim.o.shiftwidth      = 2
 
+-- Keymaps
+local nmap = function(lhs, rhs, desc)
+  vim.keymap.set('n', lhs, rhs, { desc = desc })
+end
+
+local nmap_leader = function(lhs, rhs, desc)
+  vim.keymap.set('n', '<Leader>' .. lhs, rhs, { desc = desc })
+end
+
+nmap('[p', '<Cmd>exe "put! " . v:register<CR>', 'Paste above')
+nmap(']p', '<Cmd>exe "put "  . v:register<Cr>', 'Paste below')
+nmap_leader('ei', '<Cmd>edit $MYVIMRC<CR>',     'init.lua')
