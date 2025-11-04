c776ff725f2ba9c717b5c7c31ec95143c9504bec
lew <lew@ily.rs>
2025-11-04 16:25:41 +0000

    Update .tmux.conf Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/20_keymaps.lua

--- private_dot_config/nvim/lua/config/20_keymaps.lua
+++ private_dot_config/nvim/lua/config/20_keymaps.lua
@@ -17,13 +17,5 @@ nmap_leader('ar', '<Cmd>lua vim.lsp.buf.rename()<CR>',         'lsp rename')
 nmap_leader('fi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', 'lsp find implementation')
 nmap_leader('fr', '<Cmd>lua vim.lsp.buf.references()<CR>',     'lsp find references')
 
-for i = 1, 6 do
-  nmap_leader(i, i .. "<c-w>w", "move to " .. i)
-end
 nmap_leader(';', '<Cmd>vsplit<CR>', 'vsplit')
 nmap_leader('-', '<Cmd>split<CR>', 'hsplit')
-
-nmap_leader('j', '<Cmd>resize +10<CR>', 'bigger height')
-nmap_leader('k', '<Cmd>resize -10<CR>', 'smaller height')
-nmap_leader('l', '<Cmd>vertical resize +20<CR>', 'bigger width')
-nmap_leader('h', '<Cmd>vertical resize -20<CR>', 'smaller width')
