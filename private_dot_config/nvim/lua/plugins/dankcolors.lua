deee85b67694316548584cf255569b0134066415
lew <lew@ily.rs>
2026-02-13 13:02:07 +0000

    Update .XCompose Update .gitconfig Update .tmux.conf Update .config/DankMaterialShell/firefox.css Update .config/DankMaterialShell/plugin_settings.json Update .config/DankMaterialShell/plugins/.repos/0026f1eba8dedaec/DankActions/DankActionsSettings.qml Update .config/DankMaterialShell/plugins/.repos/0026f1eba8dedaec/DankActions/DankActionsWidget.qml Update .config/DankMaterialShell/plugins/.repos/0026f1eba8dedaec/DankActions/plugin.json Update .config/DankMaterialShell/plugins/.repos/0026f1eba8dedaec/.git/index Update .config/DankMaterialShell/plugins/.repos/0026f1eba8dedaec/.git/refs/heads/master Update .config/DankMaterialShell/plugins/.repos/0026f1eba8dedaec/.git/refs/remotes/origin/master Update .config/DankMaterialShell/plugins/emojiLauncher/.git/refs/remotes/origin/main Update .config/DankMaterialShell/settings.json Update .config/alacritty/dank-theme.toml Update .config/ghostty/config Update .config/ghostty/themes/dankcolors Update .config/niri/dms/colors.kdl Update .config/nvim/lua/plugins/dankcolors.lua Update .config/pda/config.toml Update .local/bin/tmux-sessionizer

--- private_dot_config/nvim/lua/plugins/dankcolors.lua
+++ private_dot_config/nvim/lua/plugins/dankcolors.lua
@@ -4,45 +4,85 @@ return {
 		priority = 1000,
 		config = function()
 			require('base16-colorscheme').setup({
-				base00 = '#181216',
-				base01 = '#241e22',
-				base02 = '#3a3337',
-				base03 = '#4e444a',
-				base04 = '#d2c2ca',
-				base05 = '#ecdfe5',
-				base06 = '#ecdfe5',
-				base07 = '#362e33',
-				base08 = '#ffb4ab',
-				base09 = '#f5b9a4',
-				base0A = '#d2c2ca',
-				base0B = '#f4b2e2',
-				base0C = '#dcbed1',
-				base0D = '#f4b2e2',
-				base0E = '#dcbed1',
-				base0F = '#ffb4ab',
+				base00 = '#101418',
+				base01 = '#101418',
+				base02 = '#969da3',
+				base03 = '#969da3',
+				base04 = '#eff7ff',
+				base05 = '#f8fbff',
+				base06 = '#f8fbff',
+				base07 = '#f8fbff',
+				base08 = '#ff9fbc',
+				base09 = '#ff9fbc',
+				base0A = '#acd5ff',
+				base0B = '#a5ffb1',
+				base0C = '#d3e9ff',
+				base0D = '#acd5ff',
+				base0E = '#badcff',
+				base0F = '#badcff',
 			})
 
-			local function set_hl_mutliple(groups, value)
-				for _, v in pairs(groups) do vim.api.nvim_set_hl(0, v, value) end
-			end
+			vim.api.nvim_set_hl(0, 'Visual', {
+				bg = '#969da3',
+				fg = '#f8fbff',
+				bold = true
+			})
+			vim.api.nvim_set_hl(0, 'Statusline', {
+				bg = '#acd5ff',
+				fg = '#101418',
+			})
+			vim.api.nvim_set_hl(0, 'LineNr', { fg = '#969da3' })
+			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#d3e9ff', bold = true })
 
-			vim.api.nvim_set_hl(0, 'Visual',
-				{ bg = '#67355d', fg = '#ffd7f1', bold = true })
-			vim.api.nvim_set_hl(0, 'LineNr', { fg = '#4e444a' })
-			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#f4b2e2', bold = true })
+			vim.api.nvim_set_hl(0, 'Statement', {
+				fg = '#badcff',
+				bold = true
+			})
+			vim.api.nvim_set_hl(0, 'Keyword', { link = 'Statement' })
+			vim.api.nvim_set_hl(0, 'Repeat', { link = 'Statement' })
+			vim.api.nvim_set_hl(0, 'Conditional', { link = 'Statement' })
+
+			vim.api.nvim_set_hl(0, 'Function', {
+				fg = '#acd5ff',
+				bold = true
+			})
+			vim.api.nvim_set_hl(0, 'Macro', {
+				fg = '#acd5ff',
+				italic = true
+			})
+			vim.api.nvim_set_hl(0, '@function.macro', { link = 'Macro' })
+
+			vim.api.nvim_set_hl(0, 'Type', {
+				fg = '#d3e9ff',
+				bold = true,
+				italic = true
+			})
+			vim.api.nvim_set_hl(0, 'Structure', { link = 'Type' })
+
+			vim.api.nvim_set_hl(0, 'String', {
+				fg = '#a5ffb1',
+				italic = true
+			})
+
+			vim.api.nvim_set_hl(0, 'Operator', { fg = '#eff7ff' })
+			vim.api.nvim_set_hl(0, 'Delimiter', { fg = '#eff7ff' })
+			vim.api.nvim_set_hl(0, '@punctuation.bracket', { link = 'Delimiter' })
+			vim.api.nvim_set_hl(0, '@punctuation.delimiter', { link = 'Delimiter' })
+
+			vim.api.nvim_set_hl(0, 'Comment', {
+				fg = '#969da3',
+				italic = true
+			})
 
 			local current_file_path = vim.fn.stdpath("config") .. "/lua/plugins/dankcolors.lua"
-
 			if not _G._matugen_theme_watcher then
 				local uv = vim.uv or vim.loop
 				_G._matugen_theme_watcher = uv.new_fs_event()
-
 				_G._matugen_theme_watcher:start(current_file_path, {}, vim.schedule_wrap(function()
 					local new_spec = dofile(current_file_path)
-
 					if new_spec and new_spec[1] and new_spec[1].config then
 						new_spec[1].config()
-						print("󰂖 Matugen: Colors reloaded!")
+						print("Theme reload")
 					end
 				end))
 			end
