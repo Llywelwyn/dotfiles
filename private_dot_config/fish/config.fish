ec4a24bdc63582bf28883991c7a74496d59e0e40
lew <lew@ily.rs>
2026-05-21 13:37:37 +0100

    Update .config/DankMaterialShell/firefox.css Update .config/DankMaterialShell/settings.json Update .config/alacritty/dank-theme.toml Update .config/fish/config.fish Update .config/ghostty/themes/dankcolors Update .config/niri/binds.kdl Update .config/niri/dms/colors.kdl Update .config/niri/input.kdl Update .config/niri/windowrules.kdl Update .config/nvim/init.lua Update .config/nvim/snippets/package.json

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,6 +2,10 @@ set -x EDITOR nvim
 set -x PATH $HOME/.local/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 
+if test -f "$HOME/.config/fish/.env.fish"
+    source "$HOME/.config/fish/.env.fish"
+end
+
 if status is-interactive
     if test -z "$TMUX"
         if type -q tmux
@@ -28,14 +32,12 @@ if test -d "$HOME/.config/fish/"
     alias fishconf="cd $HOME/.config/fish"
 end
 
-alias dcim="cd ~/dcim"
-alias docs="cd $XDG_DOCUMENTS_DIR; or cd ~/Documents"
-alias notes="cd $XDG_DOCUMENTS_DIR/lib; or cd ~/Documents/lib; ls"
-alias dl="cd $XDG_DOWNLOAD_DIR; or cd ~/Downloads"
-alias dt="cd $XDG_DESKTOP_DIR; or cd ~/Desktop"
-alias music="cd $XDG_MUSIC_DIR; or cd ~/Music"
-alias pics="cd $XDG_PICTURES_DIR; or cd ~/Pictures"
-alias vids="cd $XDG_VIDEOS_DIR; or cd ~/Videos"
+alias docs="cd ~/documents"
+alias dl="cd ~/downloads"
+alias dt="cd ~/desktop"
+alias music="cd ~/music"
+alias pics="cd ~/pictures"
+alias vids="cd ~/videos"
 
 abbr -a -- .. 'cd ..'
 abbr -a -- ... 'cd ../..'
